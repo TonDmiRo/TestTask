650e64ee2324e2ee4240518df3ed5200352665d5
DmiRo <61103969+TonDmiRo@users.noreply.github.com>
2020-07-10 10:43:11 +0300

    Implement requests "get"

--- EmployeeDirectoryServer/EmployeeDirectoryServer/Controllers/EmployeesController.cs
+++ EmployeeDirectoryServer/EmployeeDirectoryServer/Controllers/EmployeesController.cs
@@ -17,21 +17,41 @@ namespace EmployeeDirectoryServer.Controllers {
 
         public EmployeesController(IWebHostEnvironment hostEnv) {
             string connectionString = new ConfigurationBuilder().SetBasePath(hostEnv.ContentRootPath).AddJsonFile("dbsettings.json").Build().GetConnectionString("DefaultConnection");
-            
+
             if (string.IsNullOrEmpty(connectionString))
                 throw new ArgumentException("No connection string in config.json");
 
             _employeeRepository = new EmployeeRepository(connectionString);
         }
-       
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns>
+        /// Зависит от версии. https://docs.microsoft.com/ru-ru/aspnet/core/web-api/action-return-types?view=aspnetcore-3.1
+        /// 3.0:|IEnumerable<Employee>
+        /// 2.1: Task<IEnumerable<Employee>> || IAsyncEnumerable<Employee>
+        /// </returns>
         [HttpGet]
         public async Task<IEnumerable<Employee>> Get() {
-            throw new NotImplementedException();
+            return await _employeeRepository.GetAllEmployees();
         }
         [HttpGet("{id:int}")]
-        public async Task<Employee> Get(int id) {
-            throw new NotImplementedException();
+        public async Task<ActionResult<Employee>> Get(int id) {
+            var response = await _employeeRepository.GetEmployee(id);
+            if (response == null) { return NotFound(); }
+            return response;
         }
 
+        // POST api/values
+        [HttpPost]
+        public async Task Post([FromBody] Employee value) {
+            await _employeeRepository.Insert(value);
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public async Task Delete(int id) {
+            await _employeeRepository.Delete(id);
+        }
     }
 }
