a4cc64aae800396ba4aa1b1f773aad32e3baa7d3
DmiRo <61103969+TonDmiRo@users.noreply.github.com>
2020-07-10 10:31:10 +0300

    Implement methods get

--- EmployeeDirectoryServer/EmployeeDirectoryServer.Infrastructure.Data/EmployeeRepository.cs
+++ EmployeeDirectoryServer/EmployeeDirectoryServer.Infrastructure.Data/EmployeeRepository.cs
@@ -10,37 +10,81 @@ using EmployeeDirectoryServer.Domain.Interfaces;
 
 namespace EmployeeDirectoryServer.Infrastructure.Data {
     public class EmployeeRepository : IEmployeeRepository {
-        private readonly string _connectionString;
-        public EmployeeRepository(string connectionString) {
-            this._connectionString = connectionString;
+        public async Task<IEnumerable<Employee>> GetAllEmployees() {
+            using (SqlConnection connection = new SqlConnection(_connectionString)) {
+                using (SqlCommand cmd = new SqlCommand("SELECT * FROM dbo.Employees", connection)) {
+                    // cmd.CommandType = System.Data.CommandType.StoredProcedure;
+                    var response = new List<Employee>();
+                    await connection.OpenAsync();
+
+                    using (var reader = await cmd.ExecuteReaderAsync()) {
+                        while (await reader.ReadAsync()) {
+                            response.Add(MapToEmployee(reader));
+                        }
+                    }
+
+                    return response;
+                }
+            }
         }
 
+        public async Task<Employee> GetEmployee(int id) {
+            using (SqlConnection connection = new SqlConnection(_connectionString)) {
+                using (SqlCommand cmd = new SqlCommand($"SELECT * FROM dbo.Employees WHERE ID = {id}", connection)) {
+                    // cmd.CommandType = System.Data.CommandType.StoredProcedure;
+                    // cmd.Parameters.Add(new SqlParameter("@Id", Id));
+                    Employee response = null;
+                    await connection.OpenAsync();
+
+                    using (var reader = await cmd.ExecuteReaderAsync()) {
+                        while (await reader.ReadAsync()) {
+                            response = MapToEmployee(reader);
+                        }
+                    }
+                    return response;
+                }
+            }
+        }
+
+        public Task Delete(int id) {
+            throw new NotImplementedException();
+        }
+        public Task Insert(Employee item) {
+            throw new NotImplementedException();
+        }
+
+        private Employee MapToEmployee(SqlDataReader reader) {
+            //int ID = (int)reader["ID"];
+            //string LastName = reader["LastName"].ToString();
+            //string FirstName = reader["FirstName"].ToString();
+            //string MiddleName = reader["MiddleName"].ToString();
+            //DateTime Birthday = (DateTime)reader["Birthday"];
+
+
+            return new Employee()
+            {
+                ID = (int)reader["ID"],
+                LastName = reader["LastName"].ToString(),
+                FirstName = reader["FirstName"].ToString(),
+                MiddleName = reader["MiddleName"].ToString(),
+                Birthday = (DateTime)reader["Birthday"]
+            };
+        }
+
+        // удалить?
         public void Create(Employee item) {
             throw new NotImplementedException();
         }
-
-        public void Delete(int id) {
-            throw new NotImplementedException();
-        }
-
-        public void Dispose() {
-            throw new NotImplementedException();
-        }
-
-        public IEnumerable<Employee> GetAllEmployees() {
-            throw new NotImplementedException();
-        }
-
-        public Employee GetEmployee(int id) {
-            throw new NotImplementedException();
-        }
-
         public void Save() {
             throw new NotImplementedException();
         }
-
         public void Update(Employee item) {
             throw new NotImplementedException();
+        }
+
+        private readonly string _connectionString;
+        public EmployeeRepository(string connectionString) {
+            this._connectionString = connectionString;
         }
     }
 }
