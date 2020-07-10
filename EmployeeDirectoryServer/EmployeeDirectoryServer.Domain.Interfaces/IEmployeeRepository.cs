fcebeb372a7fb026afb1907551615e0e310125b0
DmiRo <61103969+TonDmiRo@users.noreply.github.com>
2020-07-10 10:29:59 +0300

    Change interface

--- EmployeeDirectoryServer/EmployeeDirectoryServer.Domain.Interfaces/IEmployeeRepository.cs
+++ EmployeeDirectoryServer/EmployeeDirectoryServer.Domain.Interfaces/IEmployeeRepository.cs
@@ -5,11 +5,13 @@ using System.Threading.Tasks;
 using EmployeeDirectoryServer.Domain.Core;
 
 namespace EmployeeDirectoryServer.Domain.Interfaces {
-    public interface IEmployeeRepository : IDisposable {
-        IEnumerable<Employee> GetAllEmployees();
-        Employee GetEmployee(int id);
-        void Delete(int id);
+    public interface IEmployeeRepository {
+        Task<IEnumerable<Employee>> GetAllEmployees();
+        Task<Employee> GetEmployee(int id);
+        Task Insert(Employee item);
+        Task Delete(int id);
 
+        // удалить?
         void Save();
         void Update(Employee item);
         void Create(Employee item);
