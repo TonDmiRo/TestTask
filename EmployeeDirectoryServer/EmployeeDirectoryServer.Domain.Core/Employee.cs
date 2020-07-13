1bfb4f18c5e21f0aadd6f8cd772fdfd438ced229
DmiRo <61103969+TonDmiRo@users.noreply.github.com>
2020-07-13 21:56:34 +0300

    add IndexViewModel

--- EmployeeDirectoryServer/EmployeeDirectoryServer.Domain.Core/Employee.cs
+++ EmployeeDirectoryServer/EmployeeDirectoryServer.Domain.Core/Employee.cs
@@ -10,4 +10,17 @@ namespace EmployeeDirectoryServer.Domain.Core {
         public string MiddleName { get; set; }
         public DateTime Birthday { get; set; }
     }
+    public class PageInfo {
+        public int PageNumber { get; set; } // номер текущей страницы
+        public int PageSize { get; set; } // кол-во объектов на странице
+        public int TotalItems { get; set; } // всего объектов
+        public int TotalPages  // всего страниц
+        {
+            get { return (int)Math.Ceiling((decimal)TotalItems / PageSize); }
+        }
+    }
+    public class IndexViewModel {
+        public IEnumerable<Employee> Employees { get; set; }
+        public PageInfo PageInfo { get; set; }
+    }
 }
