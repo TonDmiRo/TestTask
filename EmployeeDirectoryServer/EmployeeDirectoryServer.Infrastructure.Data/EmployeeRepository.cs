0c2793e8df325d8f39bdea5f6eba41902c70a532
DmiRo <61103969+TonDmiRo@users.noreply.github.com>
2020-07-13 21:59:41 +0300

    Implement interface

--- EmployeeDirectoryServer/EmployeeDirectoryServer.Infrastructure.Data/EmployeeRepository.cs
+++ EmployeeDirectoryServer/EmployeeDirectoryServer.Infrastructure.Data/EmployeeRepository.cs
@@ -7,13 +7,36 @@ using System.Text;
 using System.Threading.Tasks;
 using EmployeeDirectoryServer.Domain.Core;
 using EmployeeDirectoryServer.Domain.Interfaces;
+using Microsoft.AspNetCore.Mvc;
 
 namespace EmployeeDirectoryServer.Infrastructure.Data {
     public class EmployeeRepository : IEmployeeRepository {
+        #region GET
+        public async Task<int> Count() {
+            using (SqlConnection connection = new SqlConnection(_connectionString)) {
+
+                string sqlRequest = $"SELECT COUNT(*) AS Count FROM dbo.Employees";
+                using (SqlCommand cmd = new SqlCommand(sqlRequest, connection)) {
+
+                    await connection.OpenAsync();
+                    var count = await cmd.ExecuteScalarAsync();
+                    int response = 0;
+                    if (count is int i) {
+                        response = i;
+                    }
+                    return response;
+                }
+            }
+        }
+        public async Task<bool> EmployeeExists(int id) {
+            var emp = await GetEmployee(id);
+            return emp != null;
+        }
         public async Task<IEnumerable<Employee>> GetAllEmployees() {
             using (SqlConnection connection = new SqlConnection(_connectionString)) {
-                using (SqlCommand cmd = new SqlCommand("SELECT * FROM dbo.Employees", connection)) {
-                    // cmd.CommandType = System.Data.CommandType.StoredProcedure;
+                string sqlRequest = "SELECT * FROM dbo.Employees";
+
+                using (SqlCommand cmd = new SqlCommand(sqlRequest, connection)) {
                     var response = new List<Employee>();
                     await connection.OpenAsync();
 
@@ -27,15 +50,58 @@ namespace EmployeeDirectoryServer.Infrastructure.Data {
                 }
             }
         }
+        public async Task<IEnumerable<Employee>> GetEmployees(int begin, int end) {
+            int count = await Count();
+            if (count <= 0) { throw new Exception("Table is empty."); }
+            if (( begin > 0 ) && ( end > begin )) {
+                if (begin < count) {
+                    string sqlRequest = "SELECT * FROM dbo.Employees WHERE ID BETWEEN @begin AND @end";
+                    using (SqlConnection connection = new SqlConnection(_connectionString)) {
+                        using (SqlCommand cmd = new SqlCommand(sqlRequest, connection)) {
 
-        public async Task<Employee> GetEmployee(int id) {
+                            cmd.Parameters.Add(new SqlParameter
+                            {
+                                ParameterName = "@begin",
+                                Value = begin,
+                                SqlDbType = SqlDbType.Int
+                            });
+                            cmd.Parameters.Add(new SqlParameter
+                            {
+                                ParameterName = "@end",
+                                Value = end,
+                                SqlDbType = SqlDbType.Int
+                            });
+
+                            var response = new List<Employee>();
+                            await connection.OpenAsync();
+                            using (var reader = await cmd.ExecuteReaderAsync()) {
+                                while (await reader.ReadAsync()) {
+                                    response.Add(MapToEmployee(reader));
+                                }
+                            }
+
+                            return response;
+                        }
+                    }
+                }
+            }
+
+            throw new NotImplementedException();
+        }
+        public async Task<Employee> GetEmployee(int employeeId) {
             using (SqlConnection connection = new SqlConnection(_connectionString)) {
-                using (SqlCommand cmd = new SqlCommand($"SELECT * FROM dbo.Employees WHERE ID = {id}", connection)) {
-                    // cmd.CommandType = System.Data.CommandType.StoredProcedure;
-                    // cmd.Parameters.Add(new SqlParameter("@Id", Id));
+                string sqlRequest = "SELECT * FROM dbo.Employees WHERE ID = @employeeId";
+                using (SqlCommand cmd = new SqlCommand(sqlRequest, connection)) {
+
+                    cmd.Parameters.Add(new SqlParameter()
+                    {
+                        ParameterName = "@employeeId",
+                        Value = employeeId,
+                        SqlDbType = SqlDbType.Int
+                    });
+
                     Employee response = null;
                     await connection.OpenAsync();
-
                     using (var reader = await cmd.ExecuteReaderAsync()) {
                         while (await reader.ReadAsync()) {
                             response = MapToEmployee(reader);
@@ -45,22 +111,89 @@ namespace EmployeeDirectoryServer.Infrastructure.Data {
                 }
             }
         }
+        #endregion
 
-        public Task Delete(int id) {
+        // POST
+        public Task<Employee> Create(Employee item) {
+            //TODO: Create
             throw new NotImplementedException();
         }
-        public Task Insert(Employee item) {
-            throw new NotImplementedException();
-        }
 
+        // PUT
+        public async Task UpdateEmployee(Employee item) {
+            using (SqlConnection connection = new SqlConnection(_connectionString)) {
+                using (SqlCommand cmd = new SqlCommand("UpdateEmployee", connection)) {
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    #region Procedure parameters
+                    // @id
+                    cmd.Parameters.Add(new SqlParameter()
+                    {
+                        ParameterName = "@id",
+                        Value = item.ID,
+                        SqlDbType = SqlDbType.Int
+                    });
+                    // @lastName
+                    cmd.Parameters.Add(new SqlParameter()
+                    {
+                        ParameterName = "@lastName",
+                       // Value = "TRUNCATE TABLE Employees",
+                        Value = item.LastName,
+                        SqlDbType = SqlDbType.NVarChar
+                    });
+                    // @firstName
+                    cmd.Parameters.Add(new SqlParameter()
+                    {
+                        ParameterName = "@firstName",
+                        Value = item.FirstName,
+                        SqlDbType = SqlDbType.NVarChar
+                    });
+                    // @middleName
+                    cmd.Parameters.Add(new SqlParameter()
+                    {
+                        ParameterName = "@middleName",
+                        Value = item.MiddleName,
+                        SqlDbType = SqlDbType.NVarChar
+                    });
+                    // @birthday
+                    cmd.Parameters.Add(new SqlParameter()
+                    {
+                        ParameterName = "@birthday",
+                        Value = item.Birthday,
+                        SqlDbType = SqlDbType.Date
+                    });
+                    #endregion
+
+                    await connection.OpenAsync();
+                    await cmd.ExecuteNonQueryAsync();
+                    return;
+                }
+            }
+        }
+       
+        public async Task Delete(int employeeId) {
+            using (SqlConnection connection = new SqlConnection(_connectionString)) {
+                string sqlRequest = $"DELETE FROM dbo.Employees ID = @employeeId";
+                using (SqlCommand cmd = new SqlCommand(sqlRequest, connection)) {
+                    cmd.Parameters.Add(new SqlParameter()
+                    {
+                        ParameterName = "@employeeId",
+                        Value = employeeId,
+                        SqlDbType = SqlDbType.Int
+                    });
+
+                    await connection.OpenAsync();
+                    await cmd.ExecuteNonQueryAsync();
+                    return;
+                }
+            }
+        }
+        #region private
         private Employee MapToEmployee(SqlDataReader reader) {
             //int ID = (int)reader["ID"];
             //string LastName = reader["LastName"].ToString();
             //string FirstName = reader["FirstName"].ToString();
             //string MiddleName = reader["MiddleName"].ToString();
             //DateTime Birthday = (DateTime)reader["Birthday"];
-
-
             return new Employee()
             {
                 ID = (int)reader["ID"],
@@ -71,20 +204,10 @@ namespace EmployeeDirectoryServer.Infrastructure.Data {
             };
         }
 
-        // удалить?
-        public void Create(Employee item) {
-            throw new NotImplementedException();
-        }
-        public void Save() {
-            throw new NotImplementedException();
-        }
-        public void Update(Employee item) {
-            throw new NotImplementedException();
-        }
-
         private readonly string _connectionString;
         public EmployeeRepository(string connectionString) {
             this._connectionString = connectionString;
         }
+        #endregion
     }
 }
